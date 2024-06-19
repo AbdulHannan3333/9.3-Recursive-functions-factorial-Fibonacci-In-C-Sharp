0d2f72f99df2de7133a68ab831d3ac3bd5ab61e9
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-19 20:47:43 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._3_Recursive_functions__factorial__Fibonacci_
+{
+    internal class Program
+    {
+        static int Factorial(int n) 
+        {
+            if (n == 0 || n==1) 
+            { 
+                return 1; 
+            } 
+            return n *Factorial(n-1);
+        }
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Console.WriteLine($"Factorial of {n}: {Factorial(n)}");
+        }
+    }
+}
